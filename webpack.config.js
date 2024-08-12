f5ebe1c9379be88bfd466490f395d468b92a406c
Tussis88 <andreab88@hotmail.it>
2024-08-12 14:17:44 +0200

    aggiunto clean a webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode:  "development",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -22,6 +23,6 @@ module.exports = {
       filename: "index.html",
       inject: "head",
       scriptLoading: "defer",
-    })
+    }),
   ],
 };
